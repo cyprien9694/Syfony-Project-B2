edda36116ea53003cb454cf7b8864d9d4ca2a435
cyprien9694 <cyprien.bidaud2005@gmail.com>
2026-01-19 15:39:37 +0100

    add

--- src/Controller/StarController.php
+++ src/Controller/StarController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Star;
+use App\Form\StarType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class StarController extends AbstractController
 {
-    #[Route('/star', name: 'app_star')]
-    public function index(): Response
+    #[Route('/star', name: 'star_list')]
+    public function index(EntityManagerInterface $em): Response
     {
+        $stars = $em->getRepository(Star::class)->findAll();
+
         return $this->render('star/index.html.twig', [
-            'controller_name' => 'StarController',
+            'stars' => $stars,
+        ]);
+    }
+
+    #[Route('/star/new', name: 'star_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $star = new Star();
+        $star->setCreatedAt(new \DateTimeImmutable()); // initialisation automatique
+
+        $form = $this->createForm(StarType::class, $star);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($star);
+            $em->flush();
+
+            $this->addFlash('success', 'Étoile ajoutée ! 🌟');
+
+            return $this->redirectToRoute('star_list');
+        }
+
+        return $this->render('star/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
