f60e809fa76bd736a921eddc593e77eafc1545c0
cyprien9694 <cyprien.bidaud2005@gmail.com>
2026-01-19 10:42:49 +0100

    edit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+// Types de champs du formulaire
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class HomeController extends AbstractController
 {
@@ -16,9 +22,43 @@ class HomeController extends AbstractController
     }
 
     #[Route('/about', name: 'app_about')]
-    public function about(): Response
+    public function about(Request $request): Response
     {
-        return $this->render('home/about.html.twig');
+        // Création du formulaire
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ])
+            ->add('subject', TextType::class, [
+                'label' => 'Sujet',
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Message',
+            ])
+            ->add('send', SubmitType::class, [
+                'label' => 'Envoyer',
+            ])
+            ->getForm();
+
+        // Traiter la soumission du formulaire
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            // Ici tu peux faire quelque chose avec les données, comme envoyer un email
+            $this->addFlash('success', 'Message envoyé !');
+
+            // Rediriger pour éviter le double envoi
+            return $this->redirectToRoute('app_about');
+        }
+
+        return $this->render('home/about.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     #[Route('/hello/{name}', name: 'app_hello')]
