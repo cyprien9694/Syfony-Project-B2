0ce6aea4c859d0b8c724d46690522b73ef076768
cyprien9694 <cyprien.bidaud2005@gmail.com>
2026-02-19 08:54:30 +0100

    ajout des test

---
+++ tests/StarTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class StarTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
